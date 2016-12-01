889d396b66c7a6cb83877f8291f9c9851ff507e5
Niti Nrw <niti.patel@flatironschool.com>
2016-12-01 15:46:45 -0500

    Hit byebug when trying to submit info

--- shallowEndrAPI/db/seeds.rb
+++ shallowEndrAPI/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+marc= User.create(username: "Marc", first_name: "Marc", last_name: "Immediato", email: "marc@me.com", password: "marcpassword")
+eoghan= User.create(username: "Eoghan", first_name: "Eoghan", last_name: "Leddy", email: "eoghan@me.com", password: "eoghanpassword")
+niti= User.create(username: "Niti", first_name: "Niti", last_name: "Patel", email: "niti@me.com", password: "nitipassword")
